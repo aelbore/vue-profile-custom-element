22ef674fdc10d28e217a56b7c09d867ca08f0639
aelbore <aelbore.sg@gmail.com>
2018-10-24 17:06:31 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,62 +2,69 @@
   <div id="app">
     <div class="container">
       <ar-input placeholder="Search Name"></ar-input>
-
-      <div class="container">
-        <div class="col-md-3" v-for="(item, index) in profiles" v-bind:key="index">
-            <Card :profile='item'  />
-        </div>
-      </div>
+      <Search :profiles='profiles' />
     </div>
   </div>
 </template>
 
 <script>
-import Card from './components/Card.vue'
+import Search from './components/Search.vue'
 import { of } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 const profiles = [
   {
+    "id": "1",
     "name": "Arjay Elbore",
     "profession": "Framework Developer",
     "motto": "I never wanted to be famous, I wanted to be great."
   },
   {
+    "id": "2",
     "name": "Aivan Monceller",
     "profession": "Developer",
     "motto": "When in doubt, iterate faster!"
   },
   {
+    "id": "2",
     "name": "Christopher Lloyd",
     "profession": "UX Developer",
     "motto": "Genius is the ability to reduce the complicated to the simple."
   },
   {
+    "id": "3",
     "name": "Martin Llaneza",
     "profession": "UI Developer",
     "motto": "The key to performance is elegance, not battalions of special cases."
   },
   {
+    "id": "4",
     "name": "Melvin Vivas",
     "profession": "Manager - Solutions Engineering",
     "motto": "When in Doubt, Iterate Faster."
   },
   {
+    "id": "5",
     "name": "Murali Pusuluri",
     "profession": "UI Developer",
     "motto": "To iterate is human, to recurse is divine."
   },
   {
+    "id": "6",
     "name": "Nox Mendina",
     "profession": "Sr. Manager - Solutions Engineering",
     "motto": "When in doubt, iterate faster!"
   }
 ];
 
+// const getProfiles = (username) => {
+//   return fetch(`https://api.github.com/users/${username}/repos`)
+//     .then(res => res.json());
+// }
+
 export default {
   name: 'app',
-  created: function() {
+  async created() {
     const self = this;
     self.profiles = profiles
 
@@ -74,11 +81,12 @@ export default {
   },
   data() {
     return {
+      loading: false,
       profiles: []
     }
   },
   components: {
-    Card
+    Search
   }
 }
 </script>
@@ -88,7 +96,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 10px;
 }
---
+++ src/components/Search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <md-table>
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Name</md-table-head>
+        <md-table-head>Profession</md-table-head>
+        <md-table-head>Motto</md-table-head>
+        <md-table-head>Actions</md-table-head>
+      </md-table-row>
+
+      <md-table-row v-for="(profile, index) in profiles" v-bind:key="index">
+        <md-table-cell md-numeric>{{ profile.id }}</md-table-cell>
+        <md-table-cell>{{ profile.name }}</md-table-cell>
+        <md-table-cell>{{ profile.profession }}</md-table-cell>
+        <md-table-cell>{{ profile.motto }}</md-table-cell>
+        <md-table-cell>
+          <md-button class="md-icon-button md-raised">
+            <md-icon>library_books</md-icon>
+          </md-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+
+    <MdTablePagination  mdPageSize="3"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+    props: ['profiles'],
+  }
+</script>
+
+
+<style scoped>
+.md-table-row:hover:not(.md-header-row) .md-table-cell{
+  background: unset!important;
+}
+</style>
